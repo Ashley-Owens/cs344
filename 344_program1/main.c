660c2d9c718064d7a155247e06d10b23e86ed3e4
Ashley-Owens <ashleyowenspt@gmail.com>
2021-09-30 14:53:14 -0700

    feat: able to read in lines from a file

--- 344_program1/main.c
+++ 344_program1/main.c
@@ -1,16 +1,17 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
-
-// int main(char *argv[]) {
+ 
+// argc is the number of inputs, char **argv are the string values 
 int main(int argc, char **argv) {
-    // argc is the number of inputs
-    // char **argv is the string values 
 
     // Pointer to the inputted filename
     char *fileName = argv[1];
+    char newline[4096];
+    int count;
 
     // Opens the file in read mode
     FILE *filePtr = fopen(fileName, "r");
@@ -19,23 +20,28 @@ int main(int argc, char **argv) {
     if (!filePtr){
             printf("Error: could not open the file %s\n", fileName);
             exit(1);
+    }
+
+    // File was valid, read it in
+    while (fgets(newline, sizeof(newline), filePtr)) {
+        if (count >= 1) {
+
+            // Returns a pointer to the new, duplicated string
+            char *temp = strdup(newline);
+            printf("Printing the newline: %s\n", temp);
         }
+        count++;
+    }
+	
 
 
-
-    // fclose(fileName);
+    // fclose(filePtr);
     
     
     // for (int i=0; i < argc; i++) {
     //     printf("arg %d : %s\n", i, argv[i]);
     // }
 
-
-    // // Open the file "grades.txt"
-    // // O_WRONLY means open the file only for writing
-    // // O_CREAT means create the file if it doesn't exit
-    // // O_TRUNC means truncate the file if it already exists
-    // // 00600 is the mode which sets file permissions that we will study later
     
     // printf("Give me a number please: ");
     // scanf("%d", &y);
