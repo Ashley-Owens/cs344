f873e75794b8ed42f47b8b436faa665d0c2d2862
Ashley-Owens <ashleyowenspt@gmail.com>
2021-10-02 14:54:09 -0700

    feat: string structs working, int/float need debugging

--- 344_program1/main.c
+++ 344_program1/main.c
@@ -8,8 +8,19 @@
 // argc is the number of inputs, char **argv are the string values 
 int main(int argc, char **argv) {
 
+    // Creates a struct for movies
+    struct movie {
+        char title[500];
+        int year;
+        char languages[500];
+        float rating;
+        struct movie *next;
+    };
+
     // Pointer to the inputted filename
     char *fileName = argv[1];
+    // Create movie struct to hold movies
+    struct movie movie_list[1000];
     char newline[4096];
     int count;
 
@@ -30,10 +41,51 @@ int main(int argc, char **argv) {
 
             // Returns a pointer to the new, duplicated string
             char *temp = strdup(newline);
-            printf("%s", temp);
+            // Uses delimitter to break up strings into tokens 
+            char *token = strtok(temp, ",\n");
+            int i = 1;
+            
+            // Adds each token to the Movie Struct list
+            // Modified from TutorialsPoint 10/2/21: https://bit.ly/3ipVjyo
+            while (token != NULL) {
+                
+                // Handles movie title
+                if(i == 1) {
+                    strcpy(movie_list[count - 1].title, token);
+                }
+
+                // Handles movie year
+                else if(i == 2) {
+                    int year = atoi(token);
+                    movie_list[count - 1].year = year;
+                }
+
+                // Handles movie languages
+                else if(i == 3) {
+                    strcpy(movie_list[count - 1].languages, token);
+                }
+
+                // Handles movie ratings
+                else if(i == 4) {
+                    movie_list[count - 1].rating = atof(token);
+                }
+
+                // printf(" %s\n", token);
+                // Sets token to Null in order to point to next token
+                token = strtok(NULL, ",\n");
+                i++;
+            }
+            // frees up memory used by temp from strdup call
+            free(temp);
         }
         count++;
     }
+    // Testing struct values
+    for (int i=0; i < count -1; i++) {
+        printf("%i: %s\n", i, movie_list[i].rating);
+    }
+    
+    printf("Processed file %s and parsed data for %d movies \n", fileName, count -1);
 	
 
 
