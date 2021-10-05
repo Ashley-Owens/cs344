25846fe3d06162627f7efce575269cbf2ed5fb0f
Ashley-Owens <ashleyowenspt@gmail.com>
2021-10-04 20:12:17 -0700

    still can't figure out how to parse strings

--- 344_program1/main.c
+++ 344_program1/main.c
@@ -14,8 +14,9 @@ struct movie {
     struct movie *next;
 };
 
-/* Parse the current line which is space delimited and create a
-*  student struct with the data in this line
+/* Parse the current line which is comma delimited and
+* create a movie struct with the data. Code adapted from:
+* https://replit.com/@cs344/studentsc#main.c
 */
 struct movie *createMovie(char *currLine) {
     struct movie *currMovie = malloc(sizeof(struct movie));
@@ -28,20 +29,22 @@ struct movie *createMovie(char *currLine) {
     currMovie->title = calloc(strlen(token) + 1, sizeof(char));
     strcpy(currMovie->title, token);
 
-    // The next token is the year, stored as integer
+    // The next token is the year, stored as an integer
     token = strtok_r(NULL, ",", &saveptr);
     currMovie->year = atoi(token);
 
-    // The next token is the array of languages
+    // The next token is the string of languages
     token = strtok_r(NULL, ",", &saveptr);
+    // memmove(&token[0], &token[1], strlen(token));
     currMovie->languages = calloc(strlen(token) + 1, sizeof(char));
     strcpy(currMovie->languages, token);
 
-    // The last token is the rating, stored as float
+    // The last token is the rating, stored as a float
     token = strtok_r(NULL, "\n", &saveptr);
+    // printf("token is %f", token);
     currMovie->rating = atof(token);
 
-    // Set the next node to NULL in the newly created student entry
+    // Set the next node to NULL in the newly created movie entry
     currMovie->next = NULL;
 
     return currMovie;
@@ -79,16 +82,15 @@ struct movie *processFile(char *filePath) {
         // Get a new movie node corresponding to the current line
         struct movie *newNode = createMovie(currLine);
 
-        // Is this the first node in the linked list?
+        // This is the first node in the linked link
+        // Sets the head and the tail to this node
         if (head == NULL) {
-            // This is the first node in the linked link
-            // Set the head and the tail to this node
             head = newNode;
             tail = newNode;
         }
+        // This is not the first node.
+        // Add this node to the list and advance the tail
         else {
-            // This is not the first node.
-            // Add this node to the list and advance the tail
             tail->next = newNode;
             tail = newNode;
         }
@@ -100,9 +102,9 @@ struct movie *processFile(char *filePath) {
     return head;
 }
 
-
 /*
-* Print data for the given movie
+* Print data for the given movie. Code adapted from:
+* https://replit.com/@cs344/studentsc#main.c
 */
 void printMovie(struct movie* aMovie){
   printf("%s, %i, %s, %.1f\n", aMovie->title,
@@ -112,12 +114,11 @@ void printMovie(struct movie* aMovie){
 }
 
 /*
-* Print the linked list of movies
+* Print the linked list of movies. Code adapted from:
+* https://replit.com/@cs344/studentsc#main.c
 */
-void printMovieList(struct movie *list)
-{
-    while (list != NULL)
-    {
+void printMovieList(struct movie *list) {
+    while (list != NULL) {
         printMovie(list);
         list = list->next;
     }
@@ -126,14 +127,12 @@ void printMovieList(struct movie *list)
 /*
 *   Process the file provided as an argument to the program to
 *   create a linked list of movie structs and prints out the list.
-*   Compile the program as follows:
-*       gcc --std=gnu99 -o movies main.c
+*   Compile the program as follows: gcc --std=gnu99 -o movies main.c
+*   Code adapted from: https://replit.com/@cs344/studentsc#main.c
 */
 int main(int argc, char **argv) {
     // argc is the number of inputs, char **argv are the string values 
-    // From example program: https://replit.com/@cs344/studentsc#main.c
-    if (argc < 2)
-    {
+    if (argc < 2) {
         printf("You must provide the name of the file to process\n");
         printf("Example usage: ./movies movie_list.csv\n");
         return EXIT_FAILURE;
@@ -143,76 +142,7 @@ int main(int argc, char **argv) {
     struct movie *list = processFile(argv[1]);
     printMovieList(list);
     return EXIT_SUCCESS;
-
-
-
-
-
-    // // Pointer to the inputted filename
-    // char *fileName = argv[1];
-    // // Create movie struct to hold movies
-    // struct movie movie_list[1000];
-    // char newline[4096];
-    // int count;
-
-    // // Opens the file in read mode
-    // FILE *filePtr = fopen(fileName, "r");
-
-    // // Exits the program if the file couldn't be opened
-    // if (!filePtr){
-    //         printf("Error: could not open the file %s\n", fileName);
-    //         exit(1);
-    // }
-
-    // // File was opened properly, reading it in
-    // while (fgets(newline, sizeof(newline), filePtr)) {
-        
-    //     // Ignores first line with Category Descriptors
-    //     if (count >= 1) {
-
-    //         // Returns a pointer to the new, duplicated string
-    //         char *temp = strdup(newline);
-    //         // Uses delimitter to break up strings into tokens 
-    //         char *token = strtok(temp, ",\n");
-    //         int i = 1;
-            
-    //         // Adds each token to the Movie Struct list
-    //         // Modified from TutorialsPoint 10/2/21: https://bit.ly/3ipVjyo
-    //         while (token != NULL) {
-                
-    //             // Handles movie title
-    //             if(i == 1) {
-    //                 strcpy(movie_list[count - 1].title, token);
-    //             }
-
-    //             // Handles movie year
-    //             else if(i == 2) {
-    //                 int year = atoi(token);
-    //                 movie_list[count - 1].year = year;
-    //             }
-
-    //             // Handles movie languages
-    //             else if(i == 3) {
-    //                 strcpy(movie_list[count - 1].languages, token);
-    //             }
-
-    //             // Handles movie ratings
-    //             else if(i == 4) {
-    //                 movie_list[count - 1].rating = atof(token);
-    //             }
-
-    //             // printf(" %s\n", token);
-    //             // Sets token to Null in order to point to next token
-    //             token = strtok(NULL, ",\n");
-    //             i++;
-    //         }
-    //         // frees up memory used by temp from strdup call
-    //         void free(void *temp);
-    //     }
-    //     count++;
-    // }
-
-   
+    
     // printf("Give me a number please: ");
     // scanf("%d", &y);
     // printf("You chose the value: %d\n", y);
