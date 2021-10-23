96fcafc7d18259d1093830d8c50fce2a81173b1d
Ashley-Owens <ashleyowenspt@gmail.com>
2021-10-23 14:16:53 -0700

    initialized smallsh project

---
+++ smallsh/main.c
@@ -0,0 +1,31 @@
+// Name: Ashley Owens
+// Date: 11/01/2021
+// Program 3: Smallsh
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+
+/* 
+*  Constant declarations: define shell input sizes
+*/
+#define CLI_MAX_LENGTH 2048;
+#define CLI_MAX_ARGS 512;
+
+
+/*
+*   Main Function: *****
+*/
+int main() {
+
+    return EXIT_SUCCESS;
+}
---
+++ smallsh/readme.txt
@@ -0,0 +1,8 @@
+Instructions for Running Program 3
+1. Unzip the assignment into your preferred directory
+2. Navigate to the folder containing the unzipped project files: owensas_program3
+3. Compile the program by entering either one of the following two commands:
+    a. gcc --std=gnu99 -o smallsh main.c
+    b. gcc --std=gnu99 -g -Wall -o smallsh main.c
+4. Run the program by entering the following command:       
+    ./smallsh
