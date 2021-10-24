ae0fb9296e5ed6a475dfd8414358de5eaa4f23ac
Ashley-Owens <ashleyowenspt@gmail.com>
2021-10-24 09:11:55 -0700

    getCommandInput receives input into an array

--- smallsh/main.c
+++ smallsh/main.c
@@ -18,14 +18,63 @@
 /* 
 *  Constant declarations: define shell input sizes
 */
-#define CLI_MAX_LENGTH 2048;
-#define CLI_MAX_ARGS 512;
+#define MAX_LENGTH 2048
+#define MAX_ARGS 512
+
+
+/* 
+*  Function declarations
+*/
+int getCommandInput(char** input, int* numOfArgs);
 
 
 /*
 *   Main Function: *****
 */
 int main() {
+    // Initializes variables for handling user input
+    char* input[MAX_ARGS];
+    int numOfArgs;
+    // char inputFilename[256];
+    // char outputFilename[256];
 
+    numOfArgs = 0;
+    memset(input, '\0', MAX_ARGS);
+    fflush(stdout);
+    fflush(stdin);
+    getCommandInput(input, &numOfArgs);
+    printf("num of args: %d\n", numOfArgs);
+    printf("input is: %s\n", *input);
+    for (int i=0; i < numOfArgs; i++) {
+        printf("input is: %s\n", input[i]);
+    }
+    printf("command input received\n");
+    
     return EXIT_SUCCESS;
+}
+
+
+int getCommandInput(char** input, int* numOfArgs) {
+    // initialize variables to help parse input
+    char buffer[MAX_ARGS];
+
+    // Prompts and adds user input into buffer array
+    printf(": ");
+    fgets(buffer, MAX_LENGTH, stdin);
+
+    // Removes the newline char
+    strtok(buffer, "\n");
+
+    // Parses first argument into input array
+    char* token = strtok(buffer, " ");
+    
+    // Continues parsing each argument and adding to array
+    while (token != NULL) {
+        input[*numOfArgs] = strdup(token);
+        printf("token is:  %s\n", token);
+        token = strtok(NULL, " ");	
+        ++*numOfArgs;
+        printf("numOfArgs is: %d\n", *numOfArgs);
+    }
+    return 0;
 }
