b66e14f745090922fc013a7a88bda41d52632f1b
Ashley-Owens <ashleyowenspt@gmail.com>
2021-10-28 15:00:13 -0700

    moved loop to runShell, working on signal handlers, ctrl+C works

--- smallsh/main.c
+++ smallsh/main.c
@@ -21,57 +21,97 @@
 #define MAX_LENGTH 2048
 #define MAX_ARGS 512
 
-
 /* 
 *  Global variable declarations for shell processing
 */
-int runInBackground = 0;                                        // Flag for background process
-char* input[MAX_ARGS];                                          // Command prompt input
-int status = 0;
-int maxChildPs = 10;                                            // Limit for number of forks
-int childPsCount = 0;                                           // Current number of child forks
-int* childPsPtr;                                                // Pointer to array storing fork PIDs
-
+int    runInBackground = 0;                                      // Flag for background process
+char*  input[MAX_ARGS];                                          // Command prompt input
+int    numOfArgs;                                                // Length of input arr
+int    status = 0;                                               // Tracks exit status
+int    maxChildPs = 10;                                          // Limit for number of forks
+int    childPsCount = 0;                                         // Current number of child forks
+int*   childPsPtr;                                               // Pointer to heap memory storing forked PIDs
 
 /* 
 *  Function declarations
 */
-int getCommandInput(int* numOfArgs);
-int parseCommandInput(int numOfArgs);
-void changeDirectory();
-void createChildProcess(int numOfArgs);
-void executeChildProcess();
-int redirect(char *path, int fromFd, int closeFd);
-void storePID(int pid);
-
-
+int   runShell();
+int   getCommandInput();
+void  changeDirectory();
+void  createChildProcess();
+void  executeChildProcess();
+int   redirect(char *path, int fromFd, int closeFd);
+void  storePID(int pid);
+int   checkPIDs();
+void  handle_SIGTSTP(int signo);
 
 /*
-*   Main Function: Runs program loop for retrieving
-*   shell input from user, parsing the input, and 
-*   performing the requested actions.
+*   Main Function: initializes signal structs, allocates
+*   heap memory for child process array, and calls
+*   helper function to run the shell.
 */
 int main() {
-    int numOfArgs;
-    bool runShellProgram = true;
-    childPsPtr = malloc(maxChildPs * sizeof(int));
     
-    while (runShellProgram) {
-        fflush(stdout);
-        fflush(stdin);
-        numOfArgs = 0;                                          // Empties input array and arg count variables
-        memset(input, '\0', MAX_LENGTH);
-        getCommandInput(&numOfArgs);
+    childPsPtr = malloc(maxChildPs * sizeof(int));              // Initializes heap space for PID array
+    struct sigaction SIGSTSTP_action = {0};                     // Initialize empty structs for signal handling
+    struct sigaction SIGINT_action = {0};
 
-        // for (int i=0; i<numOfArgs; i++) {
-        //     printf("command is: %s\n", input[i]);
-        // }
+    // Redirect ^Z to custom handle_SIGTSTP()
+    SIGSTSTP_action.sa_handler = handle_SIGTSTP;                // Register handle_SIGTSTP as the signal handler
+    sigfillset(&SIGSTSTP_action.sa_mask);                       // Block all catchable signals while handle_SIGINT is running
+    SIGSTSTP_action.sa_flags = SA_RESTART;                      // Avoids error by restarting the interrupted system call 
+    sigaction(SIGTSTP, &SIGSTSTP_action, NULL);                 // Install signal handler
 
-        runShellProgram = parseCommandInput(numOfArgs);
-        
-    }
-    free(childPsPtr);
+    // Ignore sigint signals for child processes
+    SIGINT_action.sa_handler = SIG_IGN;
+    sigfillset(&SIGINT_action.sa_mask);
+    SIGINT_action.sa_flags = 0;
+    sigaction(SIGINT, &SIGINT_action, NULL);
+
+    runShell();
+    free(childPsPtr);                                           // Frees heap memory
     return 0;
+}
+
+/*
+*   runShell()
+*   Runs the shell loop, calls helper functions to obtain
+*   command input, parse commands, and perform requested actions.
+*   Returns 0 for exit, otherwise continues running the program.
+*/
+int runShell() {
+    
+    while (true) {
+        numOfArgs = 0;
+        memset(input, '\0', MAX_LENGTH);
+        getCommandInput();
+
+        // Exits the shell: need to figure out how to kill jobs
+        if (strcmp(input[0], "exit") == 0) {
+            printf("exiting, killing jobs...\n");
+            fflush(stdout);
+            return 0;
+        }
+
+        // Changes the current working directory
+        else if (strcmp(input[0], "cd") == 0) {
+            changeDirectory();
+        }
+
+        // Figure out status...
+        else if (strcmp(input[0], "status") == 0) {
+            printf("status is true, printing out terminating signal...\n");
+            fflush(stdout);
+        }
+
+        // Creates a fork and executes commands
+        else {
+            createChildProcess();
+            fflush(stdout);
+        }
+        // Checks PID array for completed background processes
+        while (checkPIDs());
+    }
 }
 
 /*
@@ -82,14 +122,13 @@ int main() {
 * 
 *   numOfArgs: length of the command input array
 */
-int getCommandInput(int* numOfArgs) {
+int getCommandInput() {
     char buffer[MAX_LENGTH];
 
     // Command line prompt
     do {
         printf(": ");
-        
-        fgets(buffer, MAX_LENGTH +1, stdin);               // Stores input into buffer string
+        fgets(buffer, MAX_LENGTH +1, stdin);             // Stores input into buffer string
         strtok(buffer, "\n");                           // Ends token scan at the newline char
     } while (buffer[0] == '\n' || buffer[0] == '#');    // Ignores empty lines and code comments
 
@@ -109,56 +148,19 @@ int getCommandInput(int* numOfArgs) {
     // Parses buffer arguments into input char array
     char* token = strtok(buffer, " ");
     while (token != NULL) {
-        input[*numOfArgs] = strdup(token);                      // Sets the token to the next index in the input array 
+        input[numOfArgs] = strdup(token);                      // Sets the token to the next index in the input array 
         token = strtok(NULL, " ");	                    // Obtains the next token
-        ++*numOfArgs;
+        numOfArgs++;
     }
 
     // Checks for a run in the background request and sets the flag
-    if (strcmp(input[*numOfArgs-1], "&") == 0) {    
-        --*numOfArgs;
-        input[*numOfArgs] = "\0";                       // Replaces '&' with null pointer for later child processes
+    if (strcmp(input[numOfArgs-1], "&") == 0) {    
+        numOfArgs--;
+        input[numOfArgs] = "\0";                       // Replaces '&' with null pointer for later child processes
         runInBackground = 1;
     }
 
     return 0;
-}
-
-/*
-*   parseCommandInput()
-*   Iterates through user input, parses commands and calls
-*   helper functions to perform requested actions. Returns
-*   0 to "exit" or 1 to continue running the program.
-* 
-*   numOfArgs: length of the command input array
-*/
-int parseCommandInput(int numOfArgs) {
-
-    // Exits the shell: need to figure out how to kill jobs
-    if (strcmp(input[0], "exit") == 0) {
-        printf("exiting, killing jobs...\n");
-        fflush(stdout);
-        return 0;
-    }
-
-    // Changes the current working directory
-    else if (strcmp(input[0], "cd") == 0) {
-        changeDirectory();
-    }
-
-    // Figure out status...
-    else if (strcmp(input[0], "status") == 0) {
-        printf("status is true, printing out terminating signal...\n");
-        fflush(stdout);
-    }
-
-    // Creates a fork and executes commands
-    else {
-        createChildProcess(numOfArgs);
-        fflush(stdout);
-    }
-
-    return 1;
 }
 
 /*
@@ -199,7 +201,7 @@ void changeDirectory() {
 * 
 *   numOfArgs: length of the command input array
 */
-void createChildProcess(int numOfArgs) {
+void createChildProcess() {
     // Code modified from Exploration: Creating & terminating processes
     // https://bit.ly/2XOmwDV
     pid_t spawnpid = fork();                                    
@@ -215,7 +217,7 @@ void createChildProcess(int numOfArgs) {
     // Fork succeeded: child executes this code
     else if (spawnpid == 0) {
         // printf("I am the child. My pid  = %d\n", getpid());
-        executeChildProcess(numOfArgs);
+        executeChildProcess();
     }           
 
      // Parent executes this code 
@@ -242,7 +244,7 @@ void createChildProcess(int numOfArgs) {
 * 
 *   numOfArgs: length of the command input array
 */
-void executeChildProcess(int numOfArgs) {
+void executeChildProcess() {
     int     i = 0;
     int     fdsArr[2] = {-1};
 
@@ -353,4 +355,57 @@ void storePID(int pid) {
 	}
     childPsCount++;
 	childPsPtr[childPsCount] = pid;
+}
+
+int checkPIDs() {
+    int waitStatus;
+
+    for (int i=0; i < childPsCount; i++) {
+        if (waitpid(childPsPtr[i], &waitStatus, WNOHANG) != 0) {
+            
+            // print the appropriate message based on termination type
+			if (WIFEXITED(waitStatus)) {
+				printf("background pid %d is done: exit value %d\n", childPsPtr[i], WEXITSTATUS(waitStatus));
+				fflush(stdout);
+			}
+			else if (WIFSIGNALED(waitStatus)) {
+				printf("background pid %d is done: terminated by signal %d\n", childPsPtr[i], WTERMSIG(waitStatus));
+				fflush(stdout);
+			}
+            
+            // Updates the pid array by moving processes one index to the left
+			for (int j = i + 1; j < childPsCount; j++) {
+				childPsPtr[j - 1] = childPsPtr[j];
+			}
+
+			childPsCount--;
+			return 1;
+        }
+    }
+    return 0;
+}
+
+/*
+*   handle_SIGTSTP()
+*   Toggles foreground process: when SIGTSTP is called, global 
+*   varible is updated and message is written reentrantly. 
+*   Code modified from signal handling expoloration: https://bit.ly/3BqLiIh
+*/
+void handle_SIGTSTP(int signo) {
+
+    // If true, set to false and display reentrant message
+	if (runInBackground == 1) {
+		char* message = "Entering foreground-only mode (& is now ignored)\n";
+		write(1, message, 49);
+		fflush(stdout);
+		runInBackground = 0;
+	}
+
+	// If false, set to true and display reentrant message
+	else {
+		char* message = "Exiting foreground-only mode\n";
+		write (1, message, 29);
+		fflush(stdout);
+		runInBackground = 1;
+	}
 }
